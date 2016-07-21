80ae6c5ed59a2ffc086782e93a9b9dedc14809da
dhivya-rece <dhivyarajendran1996@gmail.com>
2016-07-21 15:15:36 +0530

    er
    qw

---
+++ reverse.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+int a;
+int b;
+int c;
+c=a;
+int d;
+int length=0;
+scanf("%d",&a);
+while(c>0)
+{
+c=c/10;
+++length;}
+
+while(a>0)
+{
+b=a%10;
+a=a/10;
+
+for(i=0;i<length-1;i++){
+
+d=b[i];
+}
+}
+}
