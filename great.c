a740d22a5fde94f69a476511e93b2e69f8a10fea
dhivya-rece <dhivyarajendran1996@gmail.com>
2016-07-24 17:30:20 +0530

    dfgv
    fgf

---
+++ great.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+int a,b,c;
+scanf("%d %d %d ",&a,&b,&c);
+if(a>b)
+{
+printf("first is big");
+}
+else if(b>c)
+{
+printf("sec is big")
+}
+else
+printf("third is big")
+}
+}
