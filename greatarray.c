31d388471f0516318ab7dc8d5feb19f130e59e79
dhivya-rece <dhivyarajendran1996@gmail.com>
2016-07-24 18:04:36 +0530

    erty
    tyry

---
+++ greatarray.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int a[];
+int i,j,n,c;
+
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+
+scanf("%d",&a[i]);
+c=a[0];}
+for(j=0;j<n;j++)
+{
+if(c<a[i])
+{
+c=a[i];
+}
+}
+printf("%d",c);
+}
+
