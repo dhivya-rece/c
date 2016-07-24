671ba13ca7b4e70d4539219b67c7b47e38ce1a8a
dhivya-rece <dhivyarajendran1996@gmail.com>
2016-07-24 17:24:59 +0530

    dfg
    fgf

---
+++ vow.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+char a;
+scanf("%c",&a);
+if(a=='a'|| a=='e' || a=='i'|| a=='o' || a=='u' )
+{
+printf("the value is vowel")
+}
+else{
+printf("the value is not vowel")
+}
+}
