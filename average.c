3ab0500161ff89f9d0e73139c86beb328378e21b
dhivya-rece <dhivyarajendran1996@gmail.com>
2016-07-25 18:39:44 +0530

    df
    fg

---
+++ average.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+int a[50],i,j,b=0,c=0,d=0,e,f;
+printf("enter the elements in the array");
+scanf("%d",&b);
+for(i=0;i<n;i++)
+{
+sacnf("%d",&a[i]);
+}
+for(j=0;j<n/2;j++)
+{
+c=c+a[i];
+b++;
+}
+for(i=n/2;i<n;i++)
+{
+d=d+a[i];
+}
+e=c/n;
+f=d/n;
+if(e==f){
+printf("average is possible");
+}
+else
+{
+printf("not possible");
+}
+
+}
